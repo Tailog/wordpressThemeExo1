46eaee7f5ae52bb2f5cc8523021b876c95cad04f
Santos Pierre <santospierreemmanuel@gmail.com>
2019-08-12 12:24:17 +0200

    Banner Template

--- index.php
+++ index.php
@@ -2,16 +2,8 @@
 //https://developer.wordpress.org/themes/basics/template-files/#using-template-files
 // on séparer le header dans un fichier seul pour que toutes les pages puisse l'utiliser et qu'il ne faille modifier le header qu'à un seul endroit.
 get_header();
+get_template_part('templates/banner');
 ?>
-  <!-- Header -->
-  <header class="masthead bg-primary text-white text-center">
-    <div class="container">
-      <img class="img-fluid mb-5 d-block mx-auto" src="http://localhost:8080/wp-content/themes/myTheme/img/profile.png" alt="">
-      <h1 class="text-uppercase mb-0">Start Bootstrap</h1>
-      <hr class="star-light">
-      <h2 class="font-weight-light mb-0">Web Developer - Graphic Artist - User Experience Designer</h2>
-    </div>
-  </header>
   <!-- Portfolio Grid Section -->
   <section class="portfolio" id="portfolio">
     <div class="container">

b17c683f67aa487396613db5b2ca618a54cbf87e
Santos Pierre <santospierreemmanuel@gmail.com>
2019-08-12 23:27:58 +0200

    Dynamic Menu + Adding Classes To <a> And <li>

--- functions.php
+++ functions.php
@@ -30,4 +30,48 @@ function ajout_css_js()
 // Cette écouteur va déclancher la fonction ajout_css_js()
 // https://developer.wordpress.org/reference/hooks/wp_enqueue_scripts/
 add_action('wp_enqueue_scripts', 'ajout_css_js');
+
+/**
+ * Fonction qui ajoute un menu au thème.
+ *
+ * @return void
+ */
+
+ function register_main_menu()
+{
+  register_nav_menu('main-menu', 'Menu principal dans le header.');
+}
+add_action('after_setup_theme', 'register_main_menu');
+
+/**
+ * Fonction qui ajoute des attributes au balise li des nav_menu
+ *
+*/
+function add_additional_class_on_li($classes, $item, $args)
+{
+  if ($args->add_li_class) {
+    $classes[] = $args->add_li_class;
+  }
+  return $classes;
+}
+add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
+
+/**
+ * Fonction qui ajoute des attributes au balise a des nav_menu
+ *
+ * @param [type] $att
+ * @param [type] $item
+ * @param [type] $args
+ * @return void
+ */
+function ajout_menu_a_class($atts, $item, $args)
+{
+  $class = "nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"; // or something based on $item
+  $atts['class'] = $class;
+  return $atts;
+}
+// Ajout d'un écouteur d'événement de type filtre qui nous permet de changer les attributs des balises <a>
+// les add_action et add_filter peuvent avoit jusqu'à 4 paramêtre. Le 3ème pour l'ordre d'execution et le 4 ème pour le nombre de parammètre qui seront passer à la fonction callback
+add_filter('nav_menu_link_attributes', 'ajout_menu_a_class', 10, 3); 
 ?>
+
--- header.php
+++ header.php
@@ -9,31 +9,33 @@
   <meta name="author" content="">
   <title>Freelancer - Start Bootstrap Theme</title>
   <!-- Bootstrap core CSS -->
-  <?php 
-    wp_head();
+  <?php
+  wp_head();
   ?>
 </head>
+
 <body id="page-top">
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap 2</a>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top"><?php echo get_bloginfo('name'); ?></a>
       <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a>
-          </li>
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
-          </li>
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
-          </li>
-        </ul>
+          <?php
+          $args = array(
+            // 'menu' => 'main-menu',
+            'menu_class' => 'navbar-nav ml-auto',
+            'theme_location' => 'main-menu',
+            'container'     => '',
+            'depth'         => 1,
+            'fallback_cb'   => false,
+            'add_li_class'  => 'nav-item mx-0 mx-lg-1'
+          );
+          wp_nav_menu($args);
+          ?>
       </div>
     </div>
   </nav>

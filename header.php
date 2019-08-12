37fcda9e476a5218c321c248139deffe892c98cf
Santos Pierre <santospierreemmanuel@gmail.com>
2019-08-12 15:01:35 +0200

    Import File For CSS And JS

--- header.php
+++ header.php
@@ -9,21 +9,15 @@
   <meta name="author" content="">
   <title>Freelancer - Start Bootstrap Theme</title>
   <!-- Bootstrap core CSS -->
-  <link href="<?php echo get_template_directory_uri(); ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Custom fonts for this template -->
-  <link href="<?php echo get_template_directory_uri(); ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-  <!-- Plugin CSS -->
-  <link href="<?php echo get_template_directory_uri(); ?>/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
-  <!-- Custom styles for this template -->
-  <link href="<?php echo get_template_directory_uri(); ?>/css/freelancer.min.css" rel="stylesheet">
+  <?php 
+    wp_head();
+  ?>
 </head>
 <body id="page-top">
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap 2</a>
       <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
--- index.php
+++ index.php
@@ -1,46 +1,20 @@
 <?php 
-//https://developer.wordpress.org/themes/basics/template-files/#using-template-files
-// on séparer le header dans un fichier seul pour que toutes les pages puisse l'utiliser et qu'il ne faille modifier le header qu'à un seul endroit.
-//Header
-get_header();
-//Banner
-get_template_part('templates/banner');
-//Portfolio
-get_template_part('templates/portfolio');
-//About
-get_template_part('templates/about');
-//Contact
-get_template_part('templates/contact');
+  //https://developer.wordpress.org/themes/basics/template-files/#using-template-files
+  // on séparer le header dans un fichier seul pour que toutes les pages puisse l'utiliser et qu'il ne faille modifier le header qu'à un seul endroit.
+  //Header
+  get_header();
+  //Banner
+  get_template_part('templates/banner');
+  //Portfolio
+  get_template_part('templates/portfolio');
+  //About
+  get_template_part('templates/about');
+  //Contact
+  get_template_part('templates/contact');
+  //Footer
+  get_footer();
+  //Modals
+  get_template_part('templates/portfolio', 'modal');   
 ?>
-  <!--Footer-->
-  <?php
-    get_footer();
-  ?>
-  <div class="copyright py-4 text-center text-white">
-    <div class="container">
-      <small>Copyright &copy; Your Website 2019</small>
-    </div>
-  </div>
-  <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
-  <div class="scroll-to-top d-lg-none position-fixed ">
-    <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
-      <i class="fa fa-chevron-up"></i>
-    </a>
-  </div>
-
-  <?php 
-    get_template_part('templates/portfolio', 'modal');    
-  ?>
-  <!-- Bootstrap core JavaScript -->
-  <script src="http://localhost:8080/wp-content/themes/myTheme/vendor/jquery/jquery.min.js"></script>
-  <script src="http://localhost:8080/wp-content/themes/myTheme/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- Plugin JavaScript -->
-  <script src="http://localhost:8080/wp-content/themes/myTheme/vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="http://localhost:8080/wp-content/themes/myTheme/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
-  <!-- Contact Form JavaScript -->
-  <script src="http://localhost:8080/wp-content/themes/myTheme/js/jqBootstrapValidation.js"></script>
-  <script src="http://localhost:8080/wp-content/themes/myTheme/js/contact_me.js"></script>
-  <!-- Custom scripts for this template -->
-  <script src="http://localhost:8080/wp-content/themes/myTheme/js/freelancer.min.js"></script>
 </body>
 </html>

ef349c08a02b10bbe8682ee1551d074350f1add5
Santos Pierre <santospierreemmanuel@gmail.com>
2019-08-13 13:53:33 +0200

    Adding Panel For Modification Of About Section

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  *
  * @return void
  */
-function ajout_css_js()
+function add_css_js()
 {
   // Ajout des scripts css
   // https://developer.wordpress.org/reference/functions/wp_enqueue_style/
@@ -29,7 +29,7 @@ function ajout_css_js()
 // Nous ajoutons un écouteur d'événements pour nous prévenir lorsque l'on peut ajouter des css et scripts.
 // Cette écouteur va déclancher la fonction ajout_css_js()
 // https://developer.wordpress.org/reference/hooks/wp_enqueue_scripts/
-add_action('wp_enqueue_scripts', 'ajout_css_js');
+add_action('wp_enqueue_scripts', 'add_css_js');
 
 /**
  * Fonction qui ajoute un menu au thème.
@@ -64,7 +64,7 @@ add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
  * @param [type] $args
  * @return void
  */
-function ajout_menu_a_class($atts, $item, $args)
+function add_menu_a_class($atts, $item, $args)
 {
   $class = "nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"; // or something based on $item
   $atts['class'] = $class;
@@ -72,6 +72,74 @@ function ajout_menu_a_class($atts, $item, $args)
 }
 // Ajout d'un écouteur d'événement de type filtre qui nous permet de changer les attributs des balises <a>
 // les add_action et add_filter peuvent avoit jusqu'à 4 paramêtre. Le 3ème pour l'ordre d'execution et le 4 ème pour le nombre de parammètre qui seront passer à la fonction callback
-add_filter('nav_menu_link_attributes', 'ajout_menu_a_class', 10, 3); 
+add_filter('nav_menu_link_attributes', 'add_menu_a_class', 10, 3);
+
+/**
+ * Ajout la fonctionnalité d'un ajout d'image pour les posts pour ce thème ci
+ *
+ * @return void
+ */
+function add_image_article()
+{
+  //Ajout de la fonctionnalité d'ajout d'image pour les posts pour ce thème ci
+  add_theme_support('post-thumbnails');
+  $test = 0;
+}
+// Ajout d'un écouteur d'événement pour activer les images mise en avant pour les post (article)
+add_action('init', 'add_image_article');
+
+function add_personalization_about($wp_customize){
+  // Ajout d'un panel avec des options
+  // Attention, un panel ne s'affichera que s'il contient des sections
+  //https://developer.wordpress.org/reference/classes/wp_customize_manager/add_panel/
+  $wp_customize->add_panel('coding-panel-about', [
+    'title' => __('Section About'),
+    'Description' => __('Personalization of about section')
+  ]);
+  // Ajout d'une section à un panel définie, si pas de panel défini, alors la section sera visible directement
+  // Attention il faut que la panel ait déjà été ajouter pour que la section puisse s'y greffer.
+  // Attention une section ne s'affichera que si elle contient des controls.
+  // https://developer.wordpress.org/reference/classes/wp_customize_manager/add_section/
+  $wp_customize->add_section('coding-about-section-text', [
+    'panel' => 'coding-panel-about',
+    'title' => __('Personalization of text'),
+    'description' => __('Personalization of text')
+  ]);
+
+  // Ajout d'un setting qui contiendra des informations dans la base de donnée sous la clé correspondant à son id (premier paramètre)
+  // https://developer.wordpress.org/reference/classes/wp_customize_manager/add_setting/
+  $wp_customize->add_setting('coding-about-text-left', [
+    'type' => 'theme_mod',
+    'sanitize_callback' => 'sanitize_textarea_field'
+  ]);
+  // Ajout d'un setting qui contiendra des informations dans la base de donnée sous la clé correspondant à son id (premier paramètre)
+  // La clé est utilisé pour récuperer les valeurs dans le thème grâce à la fonction get_theme_mod()
+  // Attention la ligne précédente n'est valable que si le 'type' du setting est défini à 'theme_mod'
+  // https://developer.wordpress.org/reference/classes/wp_customize_manager/add_setting/
+  $wp_customize->add_setting('coding-about-text-right', [
+    'type' => 'theme_mod',
+    'sanitize_callback' => 'sanitize_textarea_field'
+  ]);
+  // Ajout d'un control (un label avec input et autre information). Le control doit être attaché à une section ainsi qu'à un setting.
+  // Attention le setting doit déjà être créer afin que le control puisse s'ajouter.
+  // Attention un control ne s'affiche que s'il est lié à un setting.
+  // https://developer.wordpress.org/reference/classes/wp_customize_manager/add_control/
+  $wp_customize->add_control('coding-about-text-left-control', [
+    'section' => 'coding-about-section-text',
+    'settings' => 'coding-about-text-left',
+    'label' => __('Text column left'),
+    'description' => __('Personnalisez le texte de la colonne gauche'),
+    'type' => 'textarea'
+  ]);
+  $wp_customize->add_control('coding-about-text-right-control', [
+    'section' => 'coding-about-section-text',
+    'settings' => 'coding-about-text-right',
+    'label' => __('Text column right'),
+    'description' => __('Personnalisez le texte de la colonne droite'),
+    'type' => 'textarea'
+  ]);
+}
+add_action('customize_register', 'add_personalization_about'); 
+
 ?>
 
